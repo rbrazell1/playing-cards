4cd614c9b9d9455fc2e8b45f60e6bfdcda52f576
rbrazell1 <brazellrussell@gmail.com>
2021-02-11 10:45:17 -0700

    Added a class to display and shuffle a deck

--- src/main/java/edu/cnm/deepdive/model/Deck.java
+++ src/main/java/edu/cnm/deepdive/model/Deck.java
@@ -1,10 +1,12 @@
 package edu.cnm.deepdive.model;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
-public class Deck implements Dealable{
+public class Deck implements Dealable {
 
   private List<Card> cards;
 
@@ -12,7 +14,7 @@ public class Deck implements Dealable{
     cards = new ArrayList<>();
     for (Suit suit : Suit.values()) {
       for (Rank rank : Rank.values()) {
-        Card card =  new Card(rank, suit);
+        Card card = new Card(rank, suit);
         cards.add(card);
       }
     }
@@ -20,21 +22,26 @@ public class Deck implements Dealable{
 
   @Override
   public void shuffle() {
-
+    Collections.shuffle(cards);
   }
 
   @Override
   public void shuffle(Random rng) {
-
+    Collections.shuffle(cards, rng);
   }
 
   @Override
   public Dealable deal(int count) {
-    return null;
+    return null; // TODO Implement using private constructor
   }
 
   @Override
   public void sort() {
+    Collections.sort(cards);
+  }
 
+  @Override
+  public String toString() {
+    return cards.toString();
   }
 }
