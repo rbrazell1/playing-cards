c6157d72d00505350041e06f6fb63ab2935d4786
rbrazell1 <brazellrussell@gmail.com>
2021-02-16 14:02:58 -0700

    implemented comparator as anonymous

--- src/main/java/edu/cnm/deepdive/controller/Trick.java
+++ src/main/java/edu/cnm/deepdive/controller/Trick.java
@@ -63,7 +63,7 @@ public class Trick {
   }
 
   private void sort() {
-    class CardColorComparator implements Comparator<Card> {
+    Comparator<Card> comparator = new Comparator<Card>() {
 
       @Override
       public int compare(Card card1, Card card2) {
@@ -76,8 +76,7 @@ public class Trick {
         }
         return comparison;
       }
-    }
-    Comparator<Card> comparator = new CardColorComparator();
+    };
     redPile.sort(comparator);
     blackPile.sort(comparator);
 
